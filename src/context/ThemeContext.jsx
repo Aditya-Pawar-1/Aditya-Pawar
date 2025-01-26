d6c7d80f06dc045811171e27e552a1d3a7395194
aditya <pawaraditya200410@gmail.com>
2025-01-26 17:57:37 +0530

    change in theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,15 +1,13 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useContext } from 'react';
 
 const ThemeContext = createContext();
 
-export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('dark');
-
-    return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return context;
 };
 
-export const useTheme = () => useContext(ThemeContext)
+export default ThemeContext;
