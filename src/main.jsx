f36d00d24892ee00f4d0efe78a93c332a460da7c
aditya <pawaraditya200410@gmail.com>
2025-01-26 17:52:09 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './Context/ThemeContext.jsx';
+import { ThemeProvider } from './Context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
